74e1f99cd063665ee1f4b820e2db98efd4e738e6
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-09-25 21:56:41 +0530

    Implemented Authentication manager using class

---
+++ 75hrd Trainer/75hrd Trainer/AuthenticationManager.swift
@@ -0,0 +1,44 @@
+//
+//  AuthenticationManager.swift
+//  75Hrd Trainer
+//
+//  Created by Tanishq Patidar on 25/09/23.
+//
+
+import Firebase
+
+class AuthenticationManager {
+    static let shared = AuthenticationManager()
+    
+    private init() {}
+    
+    func signUp(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let user = authResult?.user {
+                let newUser = User(uid: user.uid, email: user.email)
+                completion(.success(newUser))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func signIn(email: String, password: String, completion: @escaping (Result<User, Error>) -> Void) {
+        Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+            if let user = authResult?.user {
+                let currentUser = User(uid: user.uid, email: user.email)
+                completion(.success(currentUser))
+            } else if let error = error {
+                completion(.failure(error))
+            }
+        }
+    }
+    
+    func signOut() {
+        do {
+            try Auth.auth().signOut()
+        } catch {
+            print("Error signing out: \(error.localizedDescription)")
+        }
+    }
+}

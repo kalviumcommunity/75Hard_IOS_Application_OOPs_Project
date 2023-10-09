eb4c6ef5c59306cd802059e1665181a652a83771
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-10-09 23:46:20 +0530

    used constructor to initialize firebase

--- 75hrd Trainer/75hrd Trainer/_5hrd_TrainerApp.swift
+++ 75hrd Trainer/75hrd Trainer/_5hrd_TrainerApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct _5hrd_TrainerApp: App {
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    if let sessionId = UserDefaults.standard.string(forKey: "sessionId") {
+                        print("User is logged in with session ID: \(sessionId)")
+                    } else {
+                        print("User is not logged in")
+                    }
+                }
         }
     }
 }

d911e50c7885976a65837e0862dabd85b36e66e1
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-10-24 10:56:27 +0530

    Changed the UI of Signup screen according to the new UI

--- 75hrd Trainer/75hrd Trainer/SignupScreen.swift
+++ 75hrd Trainer/75hrd Trainer/SignupScreen.swift
@@ -12,34 +12,49 @@ struct SignupScreen: View {
     @State private var email = ""
     @State private var password = ""
     @State private var confirmPassword = ""
+    @State private var isLoggedIn: Bool = false
     var body: some View {
         ZStack(alignment: .center) {
-            Color.white.edgesIgnoringSafeArea(.all)
-            VStack(spacing: 40) {
-                Image("brandLogo")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 154, height: 100.0609130859375)
-                    .clipped()
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 22) {
+                Rectangle()
+                    .frame(width: 40, height: 40)
+                    .foregroundColor(Color(red: 0.64, green: 1, blue: 0))
+                    .overlay {
+                        Image("brandLogo")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 170, height: 110)
+                            .clipped()
+                    }
+                
+                Text("Sign up")
+                    .font(
+                        Font.system(size: 24)
+                            .weight(.semibold)
+                    )
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                    .padding(.top, 24)
                 
                 TextField("Email", text: $email)
                     .disableAutocorrection(true)
                     .autocapitalization(.none)
-                    .foregroundColor(.black)
+                    .foregroundColor(Color(red: 0.57, green: 0.6, blue: 0.64))
                     .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 30)
-                            .stroke(.black, lineWidth: 1)
+                            .stroke(Color(red: 0.57, green: 0.6, blue: 0.64), lineWidth: 1)
                     )
                     .padding(.horizontal)
                 
                 
                 SecureField("Password", text: $password)
-                    .foregroundColor(.black)
+                    .foregroundColor(Color(red: 0.57, green: 0.6, blue: 0.64))
                     .padding()
                     .overlay(
                         RoundedRectangle(cornerRadius: 30)
-                            .stroke(.black, lineWidth: 1)
+                            .stroke(Color(red: 0.57, green: 0.6, blue: 0.64), lineWidth: 1)
                     )
                     .padding(.horizontal)
                 
@@ -56,20 +71,42 @@ struct SignupScreen: View {
                     Text("Sign up")
                         .frame(maxWidth: .infinity)
                         .padding()
-                        .foregroundColor(.white)
-                        .background(.blue)
-                    
+                        .foregroundColor(.black)
+                        .background(Color(red: 0.64, green: 1, blue: 0))
                         .clipShape(Capsule())
                         .padding(.horizontal)
                 })
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: 50, height: 1)
+                        .foregroundColor(Color(red: 0.57, green: 0.6, blue: 0.64))
+                    
+                    Text("OR")
+                        .foregroundColor(Color(red: 0.57, green: 0.6, blue: 0.64))
+                    
+                    Rectangle()
+                        .frame(width: 50, height: 1)
+                        .foregroundColor(Color(red: 0.57, green: 0.6, blue: 0.64))
+                }
+                NavigationLink(destination: LoginScreen(onLogin: { sessionId in
+                    UserDefaults.standard.set(sessionId, forKey: "sessionId")
+                    self.isLoggedIn = true
+                }).navigationBarHidden(true), label: {
+                    Text("Log in")
+                        .frame(maxWidth: .infinity)       .padding()
+                        .foregroundColor(.white)
+                        .background(.black)
+                        .cornerRadius(30)
+                        .overlay(RoundedRectangle(cornerRadius: 30).stroke(Color.white, lineWidth: 1))
+                        .padding(.horizontal)
+                }).navigationBarHidden(true)
             }
             .padding()
         }
     }
 }
 
-struct SignupScreen_Previews: PreviewProvider {
-    static var previews: some View {
-        SignupScreen()
-    }
+#Preview {
+    SignupScreen()
 }

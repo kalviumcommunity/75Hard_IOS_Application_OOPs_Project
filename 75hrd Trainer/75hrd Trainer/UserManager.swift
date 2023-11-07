38dc3d73974ec50fd5371e0b492bf2a562d20b04
Tanishq Patidar <tanishq.patidar@kalvium.community>
2023-11-07 08:12:13 +0530

    Imported packages and changes in save details function

--- 75hrd Trainer/75hrd Trainer/UserManager.swift
+++ 75hrd Trainer/75hrd Trainer/UserManager.swift
@@ -6,6 +6,9 @@
 //
 
 import Foundation
+import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class UserManager {
     private var currentUser: User?
@@ -14,24 +17,6 @@ class UserManager {
     
     func getCurrentUser() -> User? {
             return currentUser
-        }
-    
-    private func saveUserDetailsToFirestore(gender: String, age: Int, weight: Double, fitnessGoals: String) {
-            guard let userId = currentUser?.uid else { return }
-            
-            let userDetail = UserDetail(userId: userId, gender: gender, age: age, weight: weight, fitnessGoals: fitnessGoals)
-            
-            let db = Firestore.firestore()
-            let userDetailRef = db.collection("userDetails").document(userId)
-            
-            userDetailRef.setData(userDetail.dictionary) { error in
-                if let error = error {
-                    print("Error saving user details: \(error.localizedDescription)")
-                } else {
-                    self.userDetail = userDetail
-                    print("User details saved successfully.")
-                }
-            }
         }
     
     private func saveUserDetailsToFirestore(gender: String, age: Int, weight: Double, fitnessGoals: String) {
